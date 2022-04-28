10ce1a4fe88795d49642668b9af7010c5f7874e0
Daniel Lee <daniellee@daniels-air.home>
2022-04-28 19:11:21 -0400

    Solution to Reverse String

--- src/ReverseString/reverse-string.js
+++ src/ReverseString/reverse-string.js
@@ -3,5 +3,15 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 export const reverseString = function (s) {
-
+    let output = [];
+    
+    for(let i = 0; i < s.length; i++) {
+        output[i] = s[s.length -1 -i]
+    }
+    
+    for(let i = 0; i < s.length; i++) {
+        s[i] = output[i]
+    }
+    
+    return s
 };
