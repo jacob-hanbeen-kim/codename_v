426a03afb6b799cd6bbdb4402f20f1eecf2e8125
paul lee <paullee@pauls-MBP.attlocal.net>
2022-04-28 19:12:20 -0400

    reserver string using javascript built in function

--- src/ReverseString/reverse-string.js
+++ src/ReverseString/reverse-string.js
@@ -3,5 +3,8 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 export const reverseString = function (s) {
-
+    return s.split("").reverse().join("");
 };
+
+
+console.log(reverseString('hello'))
