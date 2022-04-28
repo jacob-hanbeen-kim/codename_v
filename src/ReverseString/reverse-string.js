57b95696cde9bb6f1002139055598941c206f2a9
jacob-hanbeen-kim <kjkjkjhan424@gmail.com>
2022-04-28 19:20:33 -0400

    reverse-string

--- src/ReverseString/reverse-string.js
+++ src/ReverseString/reverse-string.js
@@ -3,5 +3,16 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 export const reverseString = function (s) {
+    let i = 0;
+    let j = s.length - 1;
 
+    while (j > i) {
+        let temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        i++;
+        j--;
+    }
+
+    return s;
 };
